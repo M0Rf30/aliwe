f7646528f61950b2141dea6751861d846bbbc315
M0Rf30 <morfeo89@hotmail.it>
2011-01-06 22:17:54 +0100

    Reworked buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+PREFIX=usr
 make
 mkdir build
 cd build
-mkdir -p usr/bin/ && mkdir -p usr/share/aliwe
-install -m755 ../Release/aliwe usr/bin/
-cp ../models  usr/share/aliwe/
+mkdir -p $PREFIX/bin/ && mkdir -p $PREFIX/share/aliwe
+install -m755 ../Release/aliwe $PREFIX/bin/
+cp ../{models,README,COPYING,INSTALL,AUTHORS}  $PREFIX/share/aliwe/
 
